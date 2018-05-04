3217199c85d792037870c7e3753c6457c198e4b7
SCT\iruiz <Ivonne.Ruiz@ellucian.com>
2018-05-04 10:40:55 -0500

    Initial Release

---
+++ src/components/PCButton.js
@@ -0,0 +1,20 @@
+﻿import React, { Component } from 'react';
+import Button from 'material-ui/Button';
+import PropTypes from 'prop-types';
+
+class PCPutton extends Component {
+    render() {
+        const { children } = this.props;
+        return (
+            <Button variant="raised">
+                {children}
+            </Button>
+        );
+    }
+}
+
+PCPutton.propTypes = {
+    children: PropTypes.node
+};
+
+export default PCPutton;
---
+++ src/components/PCSelect.js
@@ -0,0 +1,48 @@
+﻿import React, { Component } from 'react';
+import Select from 'material-ui/Select';
+import { withStyles } from 'material-ui/styles';
+import { MenuItem } from 'material-ui/Menu';
+import { InputLabel } from 'material-ui/Input';
+import { FormControl } from 'material-ui/Form';
+import PropTypes from 'prop-types';
+
+const styles = theme => ({
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 120,
+    },
+});
+
+class PCSelect extends Component {
+    render() {
+        const { options, value, id, classes, label } = this.props;
+        let menuItems = [(<MenuItem value="" key="0"><em>None</em></MenuItem>)];
+        if (this.props.options !== undefined && this.props.options !== null && this.props.options.length > 0) {
+            menuItems.push(options.map((item, index) => (<MenuItem value={item.value} key={item.value}>{item.description}</MenuItem>)));
+        }
+        return (
+            <FormControl className={classes.formControl}>
+                <InputLabel htmlFor={id}>{label}</InputLabel>
+                <Select
+                    value={value}
+                    inputProps={{
+                        name: id,
+                        id: id,
+                    }}
+                >
+                    {menuItems}
+                </Select>
+            </FormControl>
+        );
+    }
+}
+
+PCSelect.propTypes = {
+    classes: PropTypes.object.isRequired,
+    value: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    options: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default withStyles(styles)(PCSelect);
